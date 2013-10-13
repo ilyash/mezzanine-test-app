230566614427e01027c7d9952a917510e16bd737
Ilya Sher <ilya.sher@coding-knight.com>
2013-10-13 15:49:56 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -159,7 +159,7 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "dev.db",
         # Not used with sqlite3.
         "USER": "",
         # Not used with sqlite3.
